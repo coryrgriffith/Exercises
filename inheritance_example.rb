bc91ac14fe6a7e4ed826061d4a3f5359f7f7f054
coryrgriffith <corygriffith9@gmail.com>
2018-06-20 17:32:45 -0500

    added additional methods for bike and car as well as the super method for each method

--- inheritance_example.rb
+++ inheritance_example.rb
@@ -1,4 +1,4 @@
-class Movement
+class Vehicle
 
   def initialize
     @speed = 0
@@ -19,24 +19,42 @@ class Movement
 
 end
 
-class Car < Movement
+class Car < Vehicle
   
+  attr_reader :fuel, :make, :model
+  attr_writer :fuel
+
+  def initialize(input_options)
+    super()
+    @fuel = input_options[:fuel]
+    @make = input_options[:make]
+    @model = input_options[:model]
+  end
+
   def honk_horn
     puts "Beeeeeeep!"
   end
 
 end
 
-class Bike < Movement
+class Bike < Vehicle
 
+  def initialize(input_options)
+    super()
+    @gears = input_options[:gears]
+    @type = input_options[:type]
+    @weight = input_options[:weight]
+  end
+  
   def ring_bell
     puts "Ring ring!"
   end
 end
 
-new_bike = Bike.new
-new_car = Car.new
-new_bike.accelerate
-new_car.accelerate
-new_bike.ring_bell
-new_car.honk_horn
+new_bike = Bike.new(gears: "21", type: "Mountain", weight: 20)
+new_car = Car.new(fuel: "full", make: "Ford", model: "Escape")
+# new_bike.accelerate
+# new_car.accelerate
+# new_bike.ring_bell
+# new_car.honk_horn
+
